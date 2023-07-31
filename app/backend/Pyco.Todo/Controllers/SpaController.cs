74373ce0b9f70b6dafc985fbc40e2eee91072b57
Jonas Wiedenmann <jwiedenmann@exone.de>
2023-07-31 21:33:38 +0200

    misc

--- app/backend/Pyco.Todo/Controllers/SpaController.cs
+++ app/backend/Pyco.Todo/Controllers/SpaController.cs
@@ -7,5 +7,13 @@ public class SpaController : Controller
     // Enumerate all SPA urls so we boot the Vue app correctly
     // Alternatively, use this as the 404 page so any unhandled url will boot the vue app
     [HttpGet("/")]
-    public ActionResult Index(string id) => File("/index.html", "text/html");
+    public ActionResult Index(string id)
+    {
+#if DEBUG
+        return Redirect("http://localhost:8080/");
+#else
+        return File("/index.html", "text/html");
+#endif
+
+    }
 }
