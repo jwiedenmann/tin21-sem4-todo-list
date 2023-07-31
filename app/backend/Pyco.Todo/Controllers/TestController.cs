b919bd0d0259494ea838d15df1a1b22c6454c138
Jonas Wiedenmann <jwiedenmann@exone.de>
2023-07-31 22:03:10 +0200

    wip communication to controller

---
+++ app/backend/Pyco.Todo/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pyco.Todo.Controllers
+{
+    [ApiController]
+    [Route("/api/v1/[controller]")]
+    public class TestController : Controller
+    {
+        public int Index()
+        {
+            return 42;
+        }
+    }
+}
--- app/frontend/src/views/HomeView.vue
+++ app/frontend/src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>{{ users }}</h2>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -13,6 +14,22 @@ export default {
   name: 'HomeView',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      users: 0,
+    };
+  },
+  async mounted() {
+    const { data } = await this.axios.get(
+      "https://localhost:7098/api/v1/Test"
+    ).then((r) => {
+      console.log(r.data);
+      this.albero = r.data;
+    }).catch((e) => {
+      console.log(e);
+    });;
+    this.users = data;
   }
 }
 </script>
