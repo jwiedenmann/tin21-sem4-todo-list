6e713abeab4091dc1ab11ef7496b2313310600bc
Jonas Wiedenmann <jwiedenmann@exone.de>
2023-08-03 10:17:52 +0200

    fixed routing

--- app/backend/Pyco.Todo/Controllers/SpaController.cs
+++ app/backend/Pyco.Todo/Controllers/SpaController.cs
@@ -6,7 +6,7 @@ public class SpaController : Controller
 {
     // Enumerate all SPA urls so we boot the Vue app correctly
     // Alternatively, use this as the 404 page so any unhandled url will boot the vue app
-    [HttpGet("/")]
+    [HttpGet("/todo")]
     public ActionResult Index(string id)
     {
 #if DEBUG
