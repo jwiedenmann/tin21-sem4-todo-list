ad37b75dcacaef48d6c4dc3b75482e79a0e38894
Jonas Wiedenmann <jwiedenmann@exone.de>
2023-08-03 09:59:22 +0200

    added middleware

--- app/backend/Pyco.Todo/Program.cs
+++ app/backend/Pyco.Todo/Program.cs
@@ -1,6 +1,9 @@
+using Pyco.Todo.Jwt;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddRazorPages();
 builder.Services.AddControllers();
 
 #if DEBUG
@@ -18,6 +21,12 @@ builder.Services.AddCors(options =>
 });
 #endif
 
+var jwtOptions = builder.Configuration
+    .GetSection("JwtOptions")
+    .Get<JwtOptions>();
+
+builder.Services.AddSingleton(jwtOptions);
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -27,25 +36,32 @@ if (app.Environment.IsDevelopment())
 else
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    //app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-
+//app.UseHttpsRedirection();
 app.UseDefaultFiles();
 app.UseStaticFiles();
+//app.UseCookiePolicy();
 
 app.UseRouting();
+// app.UseRateLimiter();
+// app.UseRequestLocalization();
+// app.UseCors();
 
 #if DEBUG
 app.UseCors("_myAllowSpecificOrigins");
 #endif
 
+app.UseAuthentication();
 app.UseAuthorization();
+// app.UseSession();
+// app.UseResponseCompression();
+// app.UseResponseCaching();
 
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers();
-});
+app.MapRazorPages();
+app.MapControllers();
+
+//app.MapGet("/", () => "blub");
 
 app.Run();
