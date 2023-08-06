424e9c211bed03f0d51347684cf8b4377e16a7c7
Jonas Wiedenmann <jwiedenmann@exone.de>
2023-08-06 21:36:14 +0200

    added user repo

---
+++ app/backend/Pyco.Todo.DataAccess/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using Pyco.Todo.Data.Models;
+
+namespace Pyco.Todo.DataAccess
+{
+    public interface IUserRepository
+    {
+        Task<User> Get(string username);
+    }
+}
---
+++ app/backend/Pyco.Todo.DataAccess/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using Pyco.Todo.Data.Models;
+
+namespace Pyco.Todo.DataAccess;
+
+public class UserRepository : IUserRepository
+{
+    private readonly string _connectionstring;
+
+    public UserRepository(IConfiguration configuration)
+    {
+        _connectionstring = configuration.GetConnectionString("PycoDb");
+    }
+
+    public Task<User> Get(string username)
+    {
+        const string query = @"
+select
+    id,
+    username,
+    password,
+    email,
+    archive,
+    creationDate
+from user
+where username = @username";
+
+        using var connection = new NpgsqlConnection(_connectionstring);
+        connection.Open();
+        return connection.QueryFirstAsync<User>(query, new { username });
+    }
+}
