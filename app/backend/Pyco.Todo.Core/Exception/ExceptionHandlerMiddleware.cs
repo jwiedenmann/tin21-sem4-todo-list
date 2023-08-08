b27050eecb6237c0072cdcfedde859cf4ed24347
Jonas Wiedenmann <jwiedenmann@exone.de>
2023-08-08 09:30:33 +0200

    added custom exception handling

---
+++ app/backend/Pyco.Todo.Core/Exception/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Net;
+using System.Text.Json;
+
+namespace Pyco.Todo.Core.Exception;
+
+public class ExceptionHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionHandlerMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (System.Exception error)
+        {
+            var response = context.Response;
+            response.ContentType = "application/json";
+            response.StatusCode = error switch
+            {
+                // 401 means the user needs to supply credentials, 403 means missing permissions
+                UnauthorizedException => (int)HttpStatusCode.Unauthorized,
+                // unhandled error
+                _ => (int)HttpStatusCode.InternalServerError,
+            };
+            var result = JsonSerializer.Serialize(new { message = error?.Message });
+            await response.WriteAsync(result);
+        }
+    }
+}
---
+++ app/backend/Pyco.Todo.Core/Exception/UnauthorizedException.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+namespace Pyco.Todo.Core.Exception;
+
+public class UnauthorizedException : System.Exception
+{
+    public UnauthorizedException() : base() { }
+
+    public UnauthorizedException(string message) : base(message) { }
+
+    public UnauthorizedException(string message, params object[] args)
+        : base(string.Format(CultureInfo.CurrentCulture, message, args))
+    {
+    }
+}
