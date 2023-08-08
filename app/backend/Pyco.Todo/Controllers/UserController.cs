7895253c8cd357703859a138a31c7f2eb27dd39f
Jonas Wiedenmann <jwiedenmann@exone.de>
2023-08-08 09:31:14 +0200

    added user controller

---
+++ app/backend/Pyco.Todo/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pyco.Todo.Core.Authorization.Attributes;
+using Pyco.Todo.DataAccess;
+
+namespace WebApi.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("/api/v1/[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly IUserRepository _userRepository;
+
+    public UserController(
+        IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    //[HttpGet]
+    //public async Task<IActionResult> GetAll()
+    //    => Ok(await _userRepository.GetAsync());
+
+    [HttpGet]
+    public async Task<IActionResult> GetByUsername(string username)
+        => Ok(await _userRepository.GetAsync(username));
+}
