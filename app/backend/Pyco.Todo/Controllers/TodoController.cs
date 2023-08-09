9d2ff77458a3016a26ffb6144189a8abbef04a04
Jonas Wiedenmann <jwiedenmann@exone.de>
2023-08-09 11:56:07 +0200

    renamed controller

--- app/backend/Pyco.Todo/Controllers/TodoController.cs
+++ app/backend/Pyco.Todo/Controllers/TodoController.cs
@@ -2,11 +2,11 @@
 
 namespace Pyco.Todo.Controllers;
 
-public class SpaController : Controller
+[Route("todo")]
+public class TodoController : Controller
 {
     // Enumerate all SPA urls so we boot the Vue app correctly
     // Alternatively, use this as the 404 page so any unhandled url will boot the vue app
-    [HttpGet("/todo")]
     public ActionResult Index(string id)
     {
 #if DEBUG
