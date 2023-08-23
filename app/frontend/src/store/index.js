e24087ba025a0e3000b3777b63462e11d0c87294
Jonas Wiedenmann <jwiedenmann@exone.de>
2023-08-23 10:24:53 +0200

    added jwt store

--- app/frontend/src/store/index.js
+++ app/frontend/src/store/index.js
@@ -1,11 +1,28 @@
 import { createStore } from 'vuex'
+import * as jose from 'jose'
 
 export default createStore({
   state: {
+    jwtToken: '',
+    user: {
+      id: 0,
+      username: ''
+    }
   },
   getters: {
   },
   mutations: {
+    updateJwtToken(state, newJwtToken) {
+      state.jwtToken = newJwtToken;
+
+      try {
+        const claims = jose.decodeJwt(state.jwtToken);
+        state.user.id = claims.id;
+        state.user.username = claims.username;
+      } catch (e) {
+        console.log(e);
+      }
+    }
   },
   actions: {
   },
