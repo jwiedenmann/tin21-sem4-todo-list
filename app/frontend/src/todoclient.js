ae1852c67876d99842a7a8560d9a85309befbb2f
Jonas Wiedenmann <jwiedenmann@exone.de>
2023-09-09 14:09:47 +0200

    added param object to client

--- app/frontend/src/todoclient.js
+++ app/frontend/src/todoclient.js
@@ -2,7 +2,7 @@ import store from '@/store';
 import routes from '@/constants/todoroutes'
 import axios from 'axios'
 
-export async function todo_get(route) {
+export async function todo_get(route, parameters) {
     let counter = 0;
 
     while (counter < 2) {
@@ -28,18 +28,18 @@ export async function todo_get(route) {
     }
 }
 
-async function get(route) {
+async function get(route, parameters) {
     try {
-        let resp = await axios.get(route);
+        let resp = await axios.get(route, { params: parameters });
         return resp;
     } catch (error) {
         return error.response;
     }
 }
 
-async function post(route, body) {
+async function post(route, body, parameters) {
     try {
-        let resp = await axios.post(route, body, { withCredentials: true });
+        let resp = await axios.post(route, body, { params: parameters, withCredentials: true });
         return resp;
     } catch (error) {
         return error.response;
