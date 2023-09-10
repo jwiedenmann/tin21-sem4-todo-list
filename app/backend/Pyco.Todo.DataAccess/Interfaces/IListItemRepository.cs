caf8f406839e4e20efa4e898756270ed4f46cd87
Jonas Wiedenmann <jwiedenmann@exone.de>
2023-09-10 22:29:32 +0200

    implemented listitem get

---
+++ app/backend/Pyco.Todo.DataAccess/Interfaces/IListItemRepository.cs
@@ -0,0 +1,8 @@
+﻿using Pyco.Todo.Data.Models;
+
+namespace Pyco.Todo.DataAccess.Interfaces;
+
+public interface IListItemRepository
+{
+    IEnumerable<ListItem> Get(int listId, bool showArchived = false);
+}
