7a10ea3a116d36158ab154b88ddfec7adb3e57b1
Maximilian Hartmann <max.hartmann.dischingen@gmail.com>
2023-09-12 18:04:37 +0200

    put footer in app.vue

--- app/frontend/src/views/ListOverview.vue
+++ app/frontend/src/views/ListOverview.vue
@@ -1,42 +1,103 @@
+<script>
+import { ref } from 'vue';
+import store from '@/store';
+import ListAdminVue from '../components/ListAdmin.vue'
+import { todo_get } from '@/todoclient';
+import routes from '@/constants/todoroutes'
+
+export default {
+  name: 'ListDetail',
+  components: {
+    ListAdminVue
+  },
+  data() {
+    return {
+      dataReady: false,
+      showAdminView: false,
+      componentKey: 0,
+      loggedInUser: store.state.user.username,
+      listTitle: '',
+      listUsers: [],
+      realTodoList: [],
+      todoDataList: [
+          {
+            "id": 1,
+            "title": "Meine erste Todo",
+            "creationDate": "2022-04-01",
+            "role": "admin"
+          },
+          {
+            "id": 2,
+            "title": "Noch eine Todo",
+            "creationDate": "2022-07-01",
+            "role": "admin"
+          },
+          {
+            "id": 3,
+            "title": "Hausaufgaben",
+            "creationDate": "2023-04-01",
+            "role": "user"
+          },
+          {
+            "id": 4,
+            "title": "Bucket List",
+            "creationDate": "2023-09-10",
+            "role": "admin"
+          },
+          {
+            "id": 5,
+            "title": "Einkaufsliste",
+            "creationDate": "2012-04-01",
+            "role": "readOnly"
+          }
+        ]
+    };
+  },
+  async mounted() {
+    //this.realTodoList = await todo_get(routes.LIST)
+    console.log(this.realTodoList)
+    this.dataReady = true;
+  }, methods: {
+    async openAdminView(listId){   
+      if(listId){
+        let list = await todo_get(routes.LIST, { listId: listId })
+
+        this.listTitle = list.title
+        this.listUsers = list.listUsers
+        console.log(this.listUsers)
+      }else {
+        this.listUsers = []
+        //hier noch den aktuell angemeldeten Nutzer als ListUser hinzufügen
+        this.listTitle = "New Todo List"
+      }
+      this.showAdminView = true;
+      this.forceRerenderer()
+    },
+    forceRerenderer(){
+      this.componentKey += 1
+    }
+  }
+
+}
+
+</script>
 <template>
-  <div class="list-overview">
-  <!-- <div class="lists">
-    <h3>Todos for me</h3>
-    <div class="list-group">
-      <div v-for="todo in todoDataList" v-bind:key="todo.id">
-        <a :href="'/list/' + todo.id" class="list-group-item list-group-item-action flex-column align-items-start">
-          <div class="d-flex w-100 justify-content-between">
-            <h5 class="mb-1">{{ todo.title }}</h5>
-            <small>{{ todo.creationDate }}</small>
-          </div>
-          <small>{{ todo.role }}</small>
-        </a>
-      </div>
-    </div>
-  </div> -->
   <div class="row flex-grow-1 h-75 mb-4 rounded-3" style="background-color: white;">
     <div class="container-fluid flex-column d-flex">
       <div class="row flex-grow-1">
         <div class="col-3">
           <div class="row border-end border-bottom rounded-top-3 rounded-end-0 p-3 text-light"
-            style="background-color: #54B4D3;">{{ username }}</div>
+            style="background-color: #54B4D3;">{{ loggedInUser }}</div>
           <div class="row p-4 todo-list-element" id="1" @click="selectTodoList()">Meine tolle TODO Liste</div>
           <div class="row">blub</div>
           <div class="row">blub</div>
         </div>
         <div class="col-md-auto flex-grow-1">
-
+          <ListAdminVue v-if="showAdminView" :key="componentKey" :list-title="listTitle" :list-users="listUsers"/>
         </div>
       </div>
     </div>
   </div>
-
-  <footer class="row justify-content-center pt-1 text-body-secondary border-top">
-    <p>
-      &copy; 2023
-    </p>
-  </footer>
-</div>
 </template>
 
 <style scoped>
@@ -44,15 +105,3 @@
   background-color: #f5f5f5;;
 }
 </style>
-
-<script setup>
-import { ref } from 'vue';
-import store from '@/store';
-
-const username = ref(store.state.user.username)
-
-function selectTodoList(listId) {
-  
-}
-
-</script>
