9a7bae77557418c293f8237033e3d3795bfe1bd4
Maximilian Hartmann <max.hartmann.dischingen@gmail.com>
2023-09-13 10:45:29 +0200

    fix role icons

--- app/frontend/src/components/ListUserComponent.vue
+++ app/frontend/src/components/ListUserComponent.vue
@@ -17,15 +17,15 @@ function updateRole() {
 
 <template>
     <span class="w-100 row list-group-item d-flex justify-content-between align-items-center listUserComponent">
-        <i v-if="selected === '2'" class="fa-solid fa-user col-sm-1 role-icon"></i>
-        <i v-else-if="selected === '1'" class="fa-solid fa-user-gear col-sm-1 role-icon"></i>
+        <i v-if="selected === 2" class="fa-solid fa-user col-sm-1 role-icon"></i>
+        <i v-else-if="selected === 1" class="fa-solid fa-user-gear col-sm-1 role-icon"></i>
         <i v-else class="fa-solid fa-glasses col-sm-1 role-icon"></i>
         <span class="col-sm-3">
             <select v-model="selected" class="form-select" @change="updateRole()" aria-label="Select role for user"
                 name="userRole">
-                <option value="1">Admin</option>
-                <option value="2">User</option>
-                <option value="3">Read only</option>
+                <option value=1>Admin</option>
+                <option value=2>User</option>
+                <option value=3>Read only</option>
             </select>
         </span>
         <span class="col-sm-4" id="userName">{{ userName }}</span>
